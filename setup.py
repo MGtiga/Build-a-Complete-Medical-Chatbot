38ed43e9d60c23b3c65808bec20451e6bdc77a8a
MGtiga <karankshetri@gmail.com>
2026-01-30 13:04:24 +0545

    requirements added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from gettext import install
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Karan Regmi Kshetri",
+    author_email="karankshetri321@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+
+)
